b3e03a3026a64aa16d9fd3753d011edb96eeb66b
renceabishek <renceabishek@gmail.com>
2020-10-26 12:06:02 +0530

    modified buble sort

--- src/service/Sorting.java
+++ src/service/Sorting.java
@@ -1,21 +1,25 @@
 package service;
 
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Sorting {
 
+    /**
+     * Swapping the adjustcent element if they are in wrong order.
+     *
+     * @param arr Integer.
+     */
     public void BubbleSorting(int[] arr) {
-        for(int i=0;i<arr.length;i++) {
-            for (int j=0;j<arr.length-1-i;j++) {
-                if(arr[j]>arr[j+1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
-
         printArray(arr);
     }
 
